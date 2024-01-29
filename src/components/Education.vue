e7cb50690c275c71c4db551e9c3940b6c4d842b2
Anton Cherednichenko <anton@anton.eco>
2024-01-29 12:35:05 +0100

    Responsive styles fix

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -12,8 +12,8 @@ const diplomas = t('diplomas', {returnObjects: true});
   <div>
     <div v-for="(diploma, index) in diplomas" :key="index"
          class="mb-5 grid w-full grid-cols-1 blurbox font-bold">
-      <p class="text-2xl font-bold my-5">{{ diploma.title }} ({{ diploma.date }})</p>
-      <div class="mb-6 text-lg"
+      <p class="text-2xl font-bold my-5 px-5">{{ diploma.title }} ({{ diploma.date }})</p>
+      <div class="mb-6 text-lg px-5"
            v-if="diploma.comment"
            v-html="diploma.comment"
       />
@@ -21,7 +21,7 @@ const diplomas = t('diplomas', {returnObjects: true});
           class="m-auto flex flex-wrap w-full gap-10 py-2 grid-cols-2"
       >
         <div v-for="school in diploma.school" class="m-auto flex place-content-center text-center font-bold">
-          <div class="grid grid-cols-[5rem_12rem] gap-1 max-w-72">
+          <div class="grid grid-cols-[5rem_12rem] gap-1 max-w-[90%]">
             <img :alt="school.name"
                  :src="getImage(school.logo)" class="m-auto rounded-full p-1 size-20"/>
             <p class="place-self-center">{{ school.name }} ({{ school.city }})</p>
